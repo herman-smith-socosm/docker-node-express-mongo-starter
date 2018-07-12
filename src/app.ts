892bf04565d02a793311c4f25fabb65dd7b16c3b
source-code-smith <herman@sourcecodesmith.com>
2018-07-12 13:04:27 +0200

    able to connect to db

--- src/app.ts
+++ src/app.ts
@@ -1,22 +1,61 @@
+import { Request, Response, NextFunction } from "express";
 import * as express from "express";
 import * as bodyParser from "body-parser";
 
 class App {
 
-    public app: express.Application;
+    public express: express.Application;
 
     constructor() {
-        this.app = express();
+        this.express = express();
         this.config();        
+        this.route();     
     }
 
     private config(): void{
-        // support application/json type post data
-        this.app.use(bodyParser.json());
-        //support application/x-www-form-urlencoded post data
-        this.app.use(bodyParser.urlencoded({ extended: false }));
+        this.express.use(bodyParser.json());
+        this.express.use(bodyParser.urlencoded({ extended: false }));
+        // serving static files 
+        this.express.use(express.static('public'));
     }
 
+    private route(): void{
+
+        this.express.route('/')
+        .get((req: Request, res: Response, next: NextFunction) => {
+            res.status(200).send({
+                tag: process.env.TAG,
+                message: 'UP',
+                method: 'GET'
+            })
+        })
+        .post((req: Request, res: Response, next: NextFunction) => {
+            res.status(200).send({
+                message: 'POST'
+            })
+        })
+
+        // Contact 
+        this.express.route('/contact')
+        .get((req: Request, res: Response, next: NextFunction) => {
+            // middleware
+            console.log(`Request from: ${req.originalUrl}`);
+            console.log(`Request type: ${req.method}`);
+            next();
+        }, this.contactController.getContacts)        
+
+        // POST endpoint
+        .post(this.contactController.addNewContact);
+
+        // Contact detail
+        this.express.route('/contact/:contactId')
+        // get specific contact
+        .get(this.contactController.getContactWithID)
+        .put(this.contactController.updateContact)
+        .delete(this.contactController.deleteContact)
+
+
+    }
 }
 
-export default new App().app;
+export default new App().express;
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,20 @@
-import app from "./app";
-const PORT = 3000;
+import express from "./app";
+import * as mongoose from "mongoose";
 
-app.listen(PORT, () => {
-    console.log('Express server listening on port ' + PORT);
-})
+mongoose.connect(process.env.MONGODB_URI, (err) => {
+    if (err && err instanceof mongoose.mongo.MongoError) {
+        console.log('err: ' + err);
+        
+        setTimeout(function () {
+            console.log("Retrying first connect...");
+            mongoose.connection.openUri(process.env.MONGODB_URI || '').catch(() => {});
+        }, 5 * 1000);
+    }
+});
+
+mongoose.connection.once('open', function() {
+    console.log('Connected to MongoDb');
+    express.listen(process.env.PORT, () => {
+        console.log('Express server listening on port ' + process.env.PORT);
+    })
+});
